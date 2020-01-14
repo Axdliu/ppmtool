6cf7ddeec6219f44ce43d09305e54588926128c4
Xiaodong Liu <xiaodongliu@Xiaodongs-MacBook-Pro.local>
2020-01-14 13:55:50 -0500

    added validation

--- ppmtool/src/main/java/io/kagileintelligence/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/io/kagileintelligence/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.kagileintelligence.ppmtool.services;
 
 import io.kagileintelligence.ppmtool.domain.Project;
+import io.kagileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.kagileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier((project.getProjectIdentifier().toUpperCase()));
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
